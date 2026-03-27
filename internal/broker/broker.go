6d08d0292d8bdb085cd16726a7f6be90e47be2bb
mooyg <mooybot@gmail.com>
2026-03-27 22:59:29 +0530

    feat: close operation and baseDir todo fix

--- internal/broker/broker.go
+++ internal/broker/broker.go
@@ -3,10 +3,12 @@ package broker
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"sync"
 
 	commitlog "github.com/mooyg/walrus/internal/commitlog"
 	logger "github.com/mooyg/walrus/internal/log"
+	"github.com/sirupsen/logrus"
 )
 
 type Broker struct {
@@ -18,20 +20,26 @@ type Broker struct {
 // Init scans the base directory and reopens commit logs for any topics that
 // already exist on disk, restoring broker state across restarts.
 func (b *Broker) Init() error {
-	// TODO: allow changing baseDir through arg for now keeping everything in data on-purpose
-	var baseDir string = "data"
-	b.baseDir = baseDir
+	if err := os.MkdirAll(b.baseDir, 0755); err != nil {
+		logger.Error("Failed to create baseDir", logrus.Fields{"dir": b.baseDir, "err": err})
+		return err
+	}
 
-	entries, err := os.ReadDir(baseDir)
+	entries, err := os.ReadDir(b.baseDir)
 	if err != nil {
-		logger.Error("Failed to read baseDir")
+		logger.Error("Failed to read baseDir", logrus.Fields{"dir": b.baseDir, "err": err})
+		return err
 	}
 
 	for _, e := range entries {
-		// TODO: add guard-rails for directories etc
-		l, err := commitlog.Open(e.Name())
+		if !e.IsDir() {
+			continue
+		}
+		logPath := filepath.Join(b.baseDir, e.Name())
+		l, err := commitlog.Open(logPath)
 		if err != nil {
-			logger.Error("Failed to read commitlog")
+			logger.Error("Failed to open commit log", logrus.Fields{"path": logPath, "err": err})
+			return err
 		}
 		b.topics[TopicName(e.Name())] = &Topic{log: l}
 	}
@@ -39,10 +47,10 @@ func (b *Broker) Init() error {
 	return nil
 }
 
-// NewBroker creates a Broker and restores any topics persisted from a previous run.
-func NewBroker() (*Broker, error) {
+// NewBroker creates a Broker rooted at baseDir and restores any topics persisted from a previous run.
+func NewBroker(baseDir string) (*Broker, error) {
 	b := &Broker{
-		baseDir: "data",
+		baseDir: baseDir,
 		topics:  make(map[TopicName]*Topic),
 	}
 
@@ -51,6 +59,21 @@ func NewBroker() (*Broker, error) {
 	}
 
 	return b, nil
+}
+
+// Close closes all topic logs. Should be called on shutdown.
+func (b *Broker) Close() error {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	var firstErr error
+	for name, t := range b.topics {
+		if err := t.log.Close(); err != nil && firstErr == nil {
+			logger.Error("Failed to close topic log", logrus.Fields{"topic": name, "err": err})
+			firstErr = err
+		}
+	}
+	return firstErr
 }
 
 // Produce appends data to the named topic's commit log and returns the assigned offset.
